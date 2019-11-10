fe2134ab22ccf483d4d244c07104cadebf8abc8e
henrywoody <github@henrywoody.com>
2019-11-10 12:42:31 -0800

    Adds flag of Greece
    Adds flag of Greece.

---
+++ client/src/Flags/Greece/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FlagArea from "../../Components/FlagArea";
+import "./style.css";
+
+
+const Greece: React.FC = () => {
+    return (
+        <FlagArea title="Greece"/>
+    )
+}
+
+export default Greece;
--- client/src/Flags/index.tsx
+++ client/src/Flags/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Greece from "./Greece";
 import Iceland from "./Iceland";
 import Japan from "./Japan";
 import UnitedKingdom from "./UnitedKingdom";
 
 
 const flags: React.ReactElement[] = [
+    <Greece key="greece"/>,
     <Iceland key="iceland"/>,
     <Japan key="japan"/>,
     <UnitedKingdom key="united-kingdom"/>,
