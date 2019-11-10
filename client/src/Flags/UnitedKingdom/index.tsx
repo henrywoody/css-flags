f319d7a3c8797e74f74e42b48a8463008bab61f9
henrywoody <github@henrywoody.com>
2019-11-10 12:19:11 -0800

    Adds flag of the United Kingdom
    Adds flag of the United Kingdom.

---
+++ client/src/Flags/UnitedKingdom/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FlagArea from "../../Components/FlagArea";
+import "./style.css";
+
+
+const UnitedKingdom: React.FC = () => {
+    return (
+        <FlagArea title="United Kingdom"/>
+    )
+}
+
+export default UnitedKingdom;
--- client/src/Flags/index.tsx
+++ client/src/Flags/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Iceland from "./Iceland";
 import Japan from "./Japan";
+import UnitedKingdom from "./UnitedKingdom";
 
 
 const flags: React.ReactElement[] = [
     <Iceland key="iceland"/>,
     <Japan key="japan"/>,
+    <UnitedKingdom key="united-kingdom"/>,
 ]
 
 export default flags;
