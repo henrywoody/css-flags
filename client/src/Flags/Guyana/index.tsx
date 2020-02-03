5ec10dad89c2e92238bf81e45bdd1608fd864921
henrywoody <github@henrywoody.com>
2020-02-03 00:15:33 -0800

    Adds flag of Guyana
    Adds flag of Guyana.

---
+++ client/src/Flags/Guyana/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FlagArea from "../../Components/FlagArea";
+import "./style.css";
+
+
+const Guyana: React.FC = () => {
+    return (
+        <FlagArea title="Guyana"/>
+    )
+}
+
+export default Guyana;
--- client/src/Flags/index.tsx
+++ client/src/Flags/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DemocraticRepublicOfTheCongo from "./DemocraticRepublicOfTheCongo";
 import Greece from "./Greece";
+import Guyana from "./Guyana";
 import Iceland from "./Iceland";
 import Japan from "./Japan";
 import NorthMacedonia from "./NorthMacedonia";
@@ -10,6 +11,7 @@ import UnitedKingdom from "./UnitedKingdom";
 const flags: React.ReactElement[] = [
     <DemocraticRepublicOfTheCongo key="democratic-republic-of-the-congo"/>,
     <Greece key="greece"/>,
+    <Guyana key="guyana"/>,
     <Iceland key="iceland"/>,
     <Japan key="japan"/>,
     <NorthMacedonia key="north-macedonia"/>,
