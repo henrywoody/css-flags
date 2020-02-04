b65cb83e25eece5eeb59876f505d2e9873adba6b
henrywoody <github@henrywoody.com>
2020-02-03 23:54:49 -0800

    Adds flag of Maldives
    Adds flag of Maldives.

---
+++ client/src/Flags/Maldives/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FlagArea from "../../Components/FlagArea";
+import "./style.css";
+
+
+const Maldives: React.FC = () => {
+    return (
+        <FlagArea title="Maldives"/>
+    )
+}
+
+export default Maldives;
--- client/src/Flags/index.tsx
+++ client/src/Flags/index.tsx
@@ -5,6 +5,7 @@ import Guyana from "./Guyana";
 import Iceland from "./Iceland";
 import Japan from "./Japan";
 import Laos from "./Laos";
+import Maldives from "./Maldives";
 import NorthMacedonia from "./NorthMacedonia";
 import RepublicOfArtsakh from "./RepublicOfArtsakh";
 import SaintLucia from "./SaintLucia";
@@ -20,6 +21,7 @@ const flags: React.ReactElement[] = [
     <Iceland key="iceland"/>,
     <Japan key="japan"/>,
     <Laos key="laos"/>,
+    <Maldives key="maldives"/>,
     <NorthMacedonia key="north-macedonia"/>,
     <RepublicOfArtsakh key="republic-of-artsakh"/>,
     <SaintLucia key="saint-lucia"/>,
