24e98587929fa0c37b0235beca6ba372e72cff46
henrywoody <github@henrywoody.com>
2020-03-08 00:04:53 -0800

    Adds script to create a new flag directory
    Adds a script that creates a new directory for a flag component.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CSS Flags
 
 Recreating various flags using HTML & CSS.
+
+## Adding a flag
+
+Build `utils/make_flag.go` with `go build -o utils/make_flag.exe utils/make_flag.go`. Then run `make_flage.exe` with `./make_flag.exe -o "Country Name"` then follow the instructions.
---
+++ utils/make_flag.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	var ownerName string
+	flag.StringVar(&ownerName, "o", "", "Name of the owner (state, organization, etc.) of the flag")
+	flag.Parse()
+	ownerName = strings.TrimSpace(ownerName)
+
+	if ownerName == "" {
+		panic("Please proved an owner name (-o).")
+	}
+
+	MakeFlag(ownerName)
+}
+
+const mode os.FileMode = 0755
+
+func MakeFlag(ownerName string) error {
+	fmt.Printf("Making Flag for: %s.\n", ownerName)
+
+	flagDir, err := makeFlagDirectory(ownerName)
+	if err != nil {
+		return err
+	}
+
+	err = makeFlagFiles(ownerName, flagDir)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Created flag directory for \"%s\" at: %s\n\n", ownerName, flagDir)
+
+	flagsDir, _ := path.Split(flagDir)
+	flagsIndex := path.Join(flagsDir, "index.tsx")
+	fmt.Printf("Add the following lines to %s:\n\n", flagsIndex)
+	componentName := getComponentName(ownerName)
+	key := getFlagKey(ownerName)
+	fmt.Printf("import %s from \"./%s\";\n", componentName, componentName)
+	fmt.Printf("<%s key=\"%s\"/>,\n\n", componentName, key)
+
+	return nil
+}
+
+func makeFlagDirectory(ownerName string) (string, error) {
+	ex, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	currentPath := filepath.Dir(ex)
+	projectRootPath, _ := path.Split(currentPath)
+
+	flagDir := path.Join(projectRootPath, "client", "src", "Flags", getComponentName(ownerName))
+	err = os.Mkdir(flagDir, mode)
+	if err != nil {
+		return "", err
+	}
+	return flagDir, nil
+}
+
+func makeFlagFiles(ownerName, flagDir string) error {
+	err := makeFlagIndexTSX(ownerName, flagDir)
+	if err != nil {
+		return err
+	}
+
+	err = makeFlagStyleCSS(ownerName, flagDir)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func makeFlagIndexTSX(ownerName, flagDir string) error {
+	componentName := getComponentName(ownerName)
+
+	fileContents := fmt.Sprintf(`import React from "react";
+import FlagArea from "../../Components/FlagArea";
+import DescriptionSection from "../../Components/DescriptionSection";
+import ExternalLink from "../../Components/ExternalLink";
+import "./style.css";
+
+
+const %s: React.FC = () => {
+	return (
+		<FlagArea title="%s">
+			<DescriptionSection title="Description">
+				<p>
+					{/* [DESCRIPTION] */} (Source: <ExternalLink href=""></ExternalLink>)
+				</p>
+			</DescriptionSection>
+
+			<DescriptionSection title="Construction">
+				<p>
+					{/* [Construction] */}
+				</p>
+			</DescriptionSection>
+
+			<DescriptionSection title="Sources">
+				<ul className="citation-list">
+					{ /*
+						<li>
+							[SITE]: <ExternalLink href="[URL]">"[ARTICLE]"</ExternalLink>
+						</li>
+					*/ }
+				</ul>
+			</DescriptionSection>
+		</FlagArea>
+	)
+}
+
+export default %s;
+`,
+		componentName,
+		ownerName,
+		componentName,
+	)
+	return ioutil.WriteFile(path.Join(flagDir, "index.tsx"), []byte(fileContents), 0644)
+}
+
+func makeFlagStyleCSS(ownerName, flagDir string) error {
+	flagAreaClassName := getFlagAreaCSSClassName(ownerName)
+
+	fileContents := fmt.Sprintf(`%s {
+	/* Define colors here */
+}
+
+%s .flag {
+	--aspect-ratio: calc(2 / 3);
+}
+`,
+		flagAreaClassName,
+		flagAreaClassName,
+	)
+
+	return ioutil.WriteFile(path.Join(flagDir, "style.css"), []byte(fileContents), 0644)
+}
+
+func getComponentName(ownerName string) string {
+	return toPascalCase(ownerName)
+}
+
+func getFlagAreaCSSClassName(ownerName string) string {
+	return ".flag-area-" + toKebabCase(ownerName)
+}
+
+func getFlagKey(ownerName string) string {
+	return toKebabCase(ownerName)
+}
+
+func toPascalCase(text string) string {
+	re := regexp.MustCompile(`(^|\s+)([A-Za-z])`)
+	casedText := re.ReplaceAllStringFunc(text, func(match string) string {
+		return strings.TrimSpace(strings.ToUpper(match))
+	})
+	return casedText
+}
+
+func toKebabCase(text string) string {
+	casedText := strings.ToLower(text)
+	re := regexp.MustCompile(`\s+`)
+	casedText = re.ReplaceAllString(casedText, "-")
+	return casedText
+}
